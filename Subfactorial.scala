fbd5f4115a7f90a2abe6493aaedd578765804e53
Michal Piotr Szmigiel <mp.szmigiel@gmail.com>
2014-02-26 19:46:27 +0100

    Update Subfactorial.scala

--- Subfactorial.scala
+++ Subfactorial.scala
@@ -1,5 +1,6 @@
 object Subfactorial {
   def fact(n: Int): Int = if (n == 0) 1 else n * fact(n - 1)
+  
   def subfact(n: Int): Double = math.round(fact(n) / 2.719)
 
   def main(args: Array[String]): Unit = {
